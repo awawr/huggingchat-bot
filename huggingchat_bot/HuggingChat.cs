267efe550410640f478c04e960e8850cd3165c8f
awawr <128203465+awawr@users.noreply.github.com>
2023-05-20 23:24:01 +0800

    Add files via upload

---
+++ huggingchat_bot/HuggingChat.cs
@@ -0,0 +1,58 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Support.UI;
+
+namespace huggingchat_bot
+{
+    public class HuggingChat
+    {
+        private readonly EdgeDriver _driver;
+        private readonly WebDriverWait _wdWait;
+        public string LastResponse = string.Empty;
+
+        public HuggingChat()
+        {
+            EdgeDriverService service = EdgeDriverService.CreateDefaultService();
+            service.SuppressInitialDiagnosticInformation = true;
+            service.HideCommandPromptWindow = true;
+
+            _driver = new EdgeDriver(service);
+            _driver.Url = "https://huggingface.co/chat/";
+            _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+
+            _wdWait = new WebDriverWait(_driver, TimeSpan.FromDays(365));
+        }
+
+        public void Quit()
+        {
+            _driver.Quit();
+        }
+
+        public string Ask(string text)
+        {
+        getResponse:
+            var element = _driver.FindElement(By.XPath("//textarea[@placeholder='Ask anything']"));
+            _driver.ExecuteScript($"arguments[0].value='{text.Replace("'", "\\'").Replace(":", "\\:").Replace(";", "\\;")}';", element);
+            element.SendKeys(" ");
+            element.SendKeys(Keys.Backspace);
+            element.Submit();
+
+            _wdWait.Until(d => d.FindElement(By.XPath("//*[contains(text(), 'Stop generating')]")).Displayed);
+            _wdWait.Until(d => !d.FindElement(By.XPath("//*[contains(text(), 'Stop generating')]")).Displayed);
+
+            try
+            {
+                element = _driver.FindElement(By.XPath("(//div[@class='prose max-w-none dark:prose-invert max-sm:prose-sm prose-headings:font-semibold prose-h1:text-lg prose-h2:text-base prose-h3:text-base prose-pre:bg-gray-800 dark:prose-pre:bg-gray-900'])[last()]"));
+                string response = element.Text;
+                if (response == LastResponse)
+                    throw new Exception();
+                LastResponse = response;
+                return response;
+            }
+            catch
+            {
+                goto getResponse;
+            }
+        }
+    }
+}
---
+++ huggingchat_bot/Program.cs
@@ -0,0 +1,75 @@
+﻿using Discord;
+using Discord.WebSocket;
+
+namespace huggingchat_bot
+{
+    internal class Program
+    {
+		private ulong _channelid = 1102286626379534416;
+		private string _token = "";
+		
+        private HuggingChat _chat = new HuggingChat();
+        private bool _processing = false;
+
+        private DiscordSocketClient _client = new DiscordSocketClient(
+            new DiscordSocketConfig() 
+            { 
+                GatewayIntents = GatewayIntents.AllUnprivileged | GatewayIntents.MessageContent 
+            });
+
+        public static Task Main() => new Program().MainAsync();
+
+        public async Task MainAsync()
+        {
+            _client = new DiscordSocketClient(new DiscordSocketConfig() { GatewayIntents = GatewayIntents.AllUnprivileged | GatewayIntents.MessageContent });
+            _client.Log += Log;
+            _client.MessageReceived += MessageReceived;
+
+            await _client.LoginAsync(TokenType.Bot, _token);
+            await _client.StartAsync();
+            // await _client.SetGameAsync("you sleep", type: ActivityType.Watching);
+
+            Console.ReadKey(true);
+            _chat.Quit();
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+
+        private Task MessageReceived(SocketMessage message)
+        {
+            if (message.Author.Id == _client.CurrentUser.Id)
+                return Task.CompletedTask;
+            if (message.Channel.Id != _channelid)
+                return Task.CompletedTask;
+            if (string.IsNullOrWhiteSpace(message.CleanContent))
+                return Task.CompletedTask;
+            _ = Task.Run(async () =>
+            {
+                if (_processing)
+                    return;
+                _processing = true;
+                var rmsg = await (message as SocketUserMessage).ReplyAsync("`Generating response...`");
+                try
+                {
+                    string response = _chat.Ask(message.CleanContent);
+                    if (response.Length > 1999)
+                    {
+                        response = new string(response.Take(1997).ToArray()) + "...";
+                    }
+                    await rmsg.ModifyAsync(m => m.Content = response);
+
+                }
+                catch (Exception ex)
+                {
+                    await rmsg.ModifyAsync(m => m.Content = ex.ToString());
+                }
+                _processing = false;
+            });
+            return Task.CompletedTask;
+        }
+    }
+}
